772854c0e74ccf27cd63535e4dab5c9f9430425c
JoeHowarth <josephehowarth@gmail.com>
2016-10-15 13:30:48 -0400

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -35,7 +35,7 @@ function pushPoi(location, prefDist, priceWeight, tripLocs, points){
     var poiAry = [];
     var ajaxComplete = false;
     $.ajax({
-        url: "http://api.tripadvisor.com/api/partner/2.0/map/"+ location + "/attractions?key=89DE2CFC0C1C43978B484B55F9A514EC", 
+        url: "https://api.tripadvisor.com/api/partner/2.0/map/"+ location + "/attractions?key=89DE2CFC0C1C43978B484B55F9A514EC", 
         dataType: "json",
         success: function(poiData){
             for(var i = 0; i < poiData.data.length; i++){
